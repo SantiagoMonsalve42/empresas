71f4ac1cc2592628d190911743a95d0169aff66c
santiago monsalve <santi.monsalve09@gmail.com>
2021-06-22 19:23:52 -0500

    funcion agregar persona funcionando, falta funciones CRUD experiencia y educacion

--- src/app/persona/pages/agregar/agregar.component.ts
+++ src/app/persona/pages/agregar/agregar.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+import Swal from 'sweetalert2';
+
+
+import { cargo } from 'src/app/cargo/interfaces/cargo-interface';
+import { persona } from '../../interfaces/persona-interface';
+import { Empresa } from '../../../empresa/interfaces/empresa-interface';
+import { tiposdocumentos } from 'src/app/shared/interfaces/shared-interfaces';
+import { sharedServices } from '../../../shared/services/services.service';
+import { CargosService } from '../../../cargo/services/services.service';
+import { ServiciosEmpresa } from '../../../empresa/services/services.service';
+import { PersonaService } from '../../services/services.service';
+
 
 @Component({
   selector: 'app-agregar',
@@ -6,9 +21,166 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  listaDocumentos: tiposdocumentos []=[];
+  listaCargos: cargo [] = [];
+  listaEmpresas: Empresa[]=[];
+  nuevaPersona: persona={
+    id_tipo_documento: "",
+    tipo_documento: "",
+    id_persona: "",
+    id_cargo: "",
+    id_empresa: "",
+    numero_documento: "",
+    nombre1: "",
+    nombre2: "",
+    apellido1: "",
+    apellido2: "",
+    correo: "",
+    fecha_nacimiento: "",
+    descripcion_cargo:"",
+    nombre_empresa:""
+  }
+  constructor(
+      private sharedServices: sharedServices,
+      private cargoService: CargosService,
+      private empresaService: ServiciosEmpresa,
+      private PersonaService: PersonaService,
+      private router: Router
+      ) { }
 
   ngOnInit(): void {
+    this.leerTiposDeDocumentosService();
+    this.leerCargosService();
+    this.leerEmpresasServices();
   }
 
+  limpiarFormulario(): void{
+    this.nuevaPersona={
+      id_tipo_documento: "",
+      tipo_documento: "",
+      id_persona: "",
+      id_cargo: "",
+      id_empresa: "",
+      numero_documento: "",
+      nombre1: "",
+      nombre2: "",
+      apellido1: "",
+      apellido2: "",
+      correo: "",
+      fecha_nacimiento: "yyyy-MM-dd",
+      descripcion_cargo:"",
+      nombre_empresa:""
+    }
+  }
+  
+  //alerta de sweeet alert reutilizable
+  alertaError(message: string): void{
+    Swal.fire({
+      title: 'Error!',
+      text: message,
+      icon: 'error',
+      confirmButtonText: 'Entendido'
+    })
+  }
+  
+  //alerta de sweeet alert reutilizable
+  alertaCorrecta(message:string):void{
+    Swal.fire({
+      title: 'Buen trabajo!',
+      text: message,
+      icon: 'success',
+      confirmButtonText: 'Entendido'
+    })
+  }
+
+  validarFormulario(): boolean{
+    if(this.nuevaPersona.id_tipo_documento == ""){
+      this.alertaError("Seleccione un tipo de documento");
+      return false;
+    }
+    if(this.nuevaPersona.numero_documento == ""){
+      this.alertaError("Digite un numero de documento");
+      return false;
+    }
+    if(this.nuevaPersona.nombre1 == ""){
+      this.alertaError("Digite al menos un nombre");
+      return false;
+    }
+    if(this.nuevaPersona.apellido1 == ""){
+      this.alertaError("Digite al menos un apellido");
+      return false;
+    }
+    if(this.nuevaPersona.id_empresa == ""){
+      this.alertaError("Seleccione la empresa para la cual trabaja");
+      return false;
+    }
+    if(this.nuevaPersona.id_cargo == ""){
+      this.alertaError("Seleccion un cargo");
+      return false;
+    }
+    if(this.nuevaPersona.correo == ""){
+      this.alertaError("Digite su correo electrónico");
+      return false;
+    }
+    if(this.nuevaPersona.fecha_nacimiento == "" || this.nuevaPersona.fecha_nacimiento == "yyyy-MM-dd"){
+      this.alertaError("Digite una fecha de nacimiento valida");
+      return false;
+    }
+    return true;
+  }
+
+  leerTiposDeDocumentosService(): void{
+    this.sharedServices.leerTiposDocumentos()
+        .subscribe(
+          (data) => {
+            this.listaDocumentos=data;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+  }
+  leerCargosService(): void{
+    this.cargoService.leerCargos()
+        .subscribe(
+          (data) => {
+            this.listaCargos=data;
+          },
+          (error)=>{
+            console.log(error);
+          }
+        );
+  }
+  leerEmpresasServices():void{
+    this.empresaService.leerDatos()
+        .subscribe(
+          (data) =>{
+            this.listaEmpresas=data;
+          },
+          (error) => {
+            console.log(error);
+          }
+        )
+  }
+  agregarPersonaServices():void{
+    if(this.validarFormulario()){
+      this.PersonaService.agregarPersona(this.nuevaPersona)
+          .subscribe(
+            (data) => {
+              if(data == null){
+                this.alertaCorrecta('Se creó la empresa correctamente.');
+                this.limpiarFormulario();
+                this.router.navigate(["personas"]);
+              }
+            },
+            (error) => {
+              if(error.status == 400){
+                console.log(error);
+                this.alertaError('Error al crear la empresa, intentelo mas tarde');
+              }
+            }
+          );
+    }
+    
+  }
 }
--- src/app/persona/services/services.service.ts
+++ src/app/persona/services/services.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
-import { persona } from '../interfaces/persona-interface';
+import { experiencia, persona } from '../interfaces/persona-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,37 @@ export class PersonaService {
   leerPersonas(): Observable<persona[]>{
    const url=this.url+"/persona";
    return this.http.get<persona[]>(url);
+  }
+
+  agregarPersona(persona: persona): Observable<persona>{
+    const url=this.url+"/persona";
+    const params = new HttpParams()
+      .set('id_td',persona.id_tipo_documento)
+      .set('id_car',persona.id_cargo)
+      .set('id_emp',persona.id_empresa)
+      .set('ndoc',persona.numero_documento)
+      .set('n1',persona.nombre1)
+      .set('n2',persona.nombre2)
+      .set('a1',persona.apellido1)
+      .set('a2',persona.apellido2)
+      .set('mail',persona.correo)
+      .set('fnac',persona.fecha_nacimiento);
+
+    return this.http.post<persona>(url,params);
+    
+  }
+
+  leerPersonaPorID(id: any): Observable<persona>{
+    const url=this.url+"/persona";
+    const params = new HttpParams()
+          .set('id',id);
+   return this.http.get<persona>(url,{params});
+  }
+
+  leerExperiencias(id: string): Observable<experiencia[]>{
+    const url=this.url+"/experiencia";
+    const params = new HttpParams()
+          .set('id_p',id);
+    return this.http.get<experiencia[]>(url,{params});     
   }
 }
